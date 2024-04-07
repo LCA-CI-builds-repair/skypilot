e2ae70e08bd0c74964479eaa42ba14a21a222d20
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:35:05 +0000

    test_lca_code_editing__code-editing-metrics-668819e8502524f7__id_35

--- sky/resources.py
+++ sky/resources.py
@@ -843,7 +843,6 @@ class Resources:
                         f'{size_compare} the specified disk_size: '
                         f'{self.disk_size} GB. Please specify a larger '
                         'disk_size to use this image.')
-
     def _try_validate_disk_tier(self) -> None:
         if self.disk_tier is None:
             return
