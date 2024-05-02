e85719de856f448b2a7d8618ee824e6e325f4884
ubuntu <ubuntu@ip-10-0-0-215>
2024-05-02 11:45:20 +0000

    test_lca_code_editing__code-editing-metrics-cdfcab645d0a57a__id_36

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,15 +90,21 @@ copybutton_prompt_is_regexp = True
 
 html_show_sourcelink = False
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = '_static/SkyPilot_wide_light.svg'
-
-
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico), 16x16 or 32x32 pixels.
 html_favicon = '_static/favicon.ico'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/SkyPilot_wide_light.svg'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs. This file should be a Windows icon file (.ico), 16x16 or 32x32 pixels.
+html_favicon = '_static/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
