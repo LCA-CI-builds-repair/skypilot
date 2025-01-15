becef55174361a095818cff31ee0fd7fccac2851
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-15 16:40:00 +0000

    waleko__gpt-4o_8b2a__id_35

--- sky/resources.py
+++ sky/resources.py
@@ -106,7 +106,7 @@ class Resources:
           spot_recovery: the spot recovery strategy to use for the managed
             spot to recover the cluster from preemption. Refer to
             `recovery_strategy module <https://github.com/skypilot-org/skypilot/blob/master/sky/spot/recovery_strategy.py>`__ # pylint: disable=line-too-long
-            for more details.
+                        for more details.
           region: the region to use.
           zone: the zone to use.
           image_id: the image ID to use. If a str, must be a string
