7928e663226e8ca8e6c75ec47fae768dcb877683
ubuntu <ubuntu@ip-10-0-1-88>
2025-01-24 20:03:52 +0000

    waleko__gpt-4o_e69e__id_35

--- sky/resources.py
+++ sky/resources.py
@@ -839,10 +839,9 @@ class Resources:
                     size_compare = 'larger than' if image_size > self.disk_size \
                         else 'equal to'
                     raise ValueError(
-                        f'Image {image_id!r} is {image_size}GB, which is '
-                        f'{size_compare} the specified disk_size: '
-                        f'{self.disk_size} GB. Please specify a larger '
-                        'disk_size to use this image.')
+                        f'Image {image_id!r} is {image_size}GB, which is {size_compare} '
+                        f'the specified disk_size: {self.disk_size} GB. Please specify '
+                        'a larger disk_size to use this image.')
 
     def _try_validate_disk_tier(self) -> None:
         if self.disk_tier is None:
